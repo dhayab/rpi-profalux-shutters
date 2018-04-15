84c9567108a0a83bfe0fc5ac79006739f9515463
Dhaya Benmessaoud <dhaya.benmessaoud@gmail.com>
2018-04-15 14:05:54 +0200

    chore: allow custom config path + fake gpio for dev environment

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const config = require('./config.json');
+
+const argv = require('minimist')(process.argv.slice(2));
+const config = ((configName) => {
+    const configPath = require('path').join(process.cwd(), configName);
+    if (!require('fs').existsSync(configPath)) {
+        console.error(`Config file "${configPath}" could not be found.`); //eslint-disable-line no-console
+        process.exit(127);
+    }
+    return require(configPath);
+})(argv.config || 'config.json');
 
 const Server = require('./lib/server');
 const Shutter = require('./lib/shutter');
